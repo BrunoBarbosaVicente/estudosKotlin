1712858bc2b3a72a1561939966f56eeca1339c4b
Mon4rka <brunobarbosavicente@hotmail.com>
2026-08-02 16:58:36 -0300

    feat: adiciona exercicio when

--- exercicios/semanaDois/executarMesmoBloco.kt
+++ exercicios/semanaDois/executarMesmoBloco.kt
@@ -1,5 +1,7 @@
 fun main() {
-    when (...){
-        ?, ? -> println("Entrada permitida")
+    val mes = 1
+    when (mes){
+        1, 2, 3 -> println("Entrada permitida")
+        else -> println("Entrada não permitida")
     }
 }
