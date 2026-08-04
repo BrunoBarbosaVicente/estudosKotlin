c62cc51057714ee5cade2134e57578e5591ca676
BrunoBarbosaVicente <brunobarbosavicente@hotmail.com>
2026-08-03 21:15:20 -0300

    refactor: melhoria do menuInterativo

--- exercicios/semanaDois/menuInterativo.kt
+++ exercicios/semanaDois/menuInterativo.kt
@@ -5,33 +5,31 @@ fun main() {
     println("2 - Saque")
     println("3 - Depósito")
     val opcao = readln().toInt()
+    val saldo = 1000.0
     when (opcao) {
         1 -> {
-            val saldo = 1000.0
-            println("Seu saldo atual é: R$ $saldo")
+            println("Seu saldo atual é: R$ %.2f".format(saldo))
         }
         2 -> {
             println("Digite o valor que deseja sacar:")
-            val saldo = 1000.0
             val valorSaque = readln().toDouble()
             when {
                 valorSaque > saldo -> println("Saldo insuficiente para realizar o saque.")
                 valorSaque <= 0 -> println("valor invalido. Por favor, digite um valor positivo.")
                 else -> {
                     val saque = saldo - valorSaque
-                    println("Saque realizado com sucesso! Seu saldo atual é: R$ $saque")
+                    println("Saque realizado com sucesso! Seu saldo atual é: R$ %.2f".format(saque))
                 }
             }
         }
         3 -> {
             println("Digite o valor que deseja depositar:")
-            val saldo = 1000.0
             val valorDeposito = readln().toDouble()
             when {
                 valorDeposito <= 0 -> println("valor invalido. Por favor, digite um valor positivo.")
                 else -> {
                     val deposito = saldo + valorDeposito
-                    println("Deposito realizado com sucesso! Seu saldo atual é R$ $deposito")
+                    println("Deposito realizado com sucesso! Seu saldo atual é R$ %.2f".format(deposito))
                 }
             }
         }
