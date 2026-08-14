02fdae8391d2748f50a286327701ce254ef469aa
Mon4rka <brunobarbosavicente@hotmail.com>
2026-08-14 16:02:08 -0300

    feat: projeto semana 3 finalizado

--- projetos/semana3/sistemaBancario.kt
+++ projetos/semana3/sistemaBancario.kt
@@ -1,35 +1,45 @@
-var saldoConta = 1000.0
-fun calculdora (operador: String, num1: Double, num2: Double): String {
-    when (operador) {
-        "x" -> println(num1 * num2)
-        "/" -> println(num1 / num2)
-        "+" -> println(num1 + num2)
-        "-" -> println(num1 - num2)
-        else -> println("operador invalido")
-    }
-    return operador
-}
-fun consultarSaldo (saldo: Double): String {
-    return "Seu saldo é $saldo"
-}
-fun sacar (saque: Double) {
-    saldoConta -= saque
-}
-
-
-
 fun main (){
-    println("====================")
-    println(   "Banco Kotlin"   )
-    println("====================")
-    println("")
-    println("1 - Consultar saldo")
-    println("2 - Sacar")
-    println("3 - Depositar")
-    println("0 - Sair")
-    println("")
-    println("Escolha um opção:")
-    val opcao = readln().toInt()
-    if ()
-    
+    var saldoConta = 1000.0
+    do {
+        println("====================")
+        println("    Banco Kotlin    ")
+        println("====================")
+        println("")
+        println("1 - Consultar saldo")
+        println("2 - Sacar")
+        println("3 - Depositar")
+        println("0 - Sair")
+        println("")
+        println("Escolha um opção:")
+        var opcao = readln().toInt()
+        when (opcao) {
+            1 -> { println("Seu saldo é de $saldoConta")
+        }
+            2 -> { println("Quanto deseja sacar?")
+                 val saque = readln().toDouble()
+                 if (saque > saldoConta){
+                    println("Saldo insuficiente")
+                    } else {
+                        saldoConta -= saque
+                        println("Saque realizado com sucesso")
+                        println("Seu novo saldo é de $saldoConta")
+                    }
+                }
+            3 -> { println("Quanto deseja depositar?")
+                    val deposito = readln().toDouble()
+                    if (deposito <= 0) {
+                    println("Valor invalido")
+                    } else {
+                        saldoConta += deposito
+                        println("Seu novo saldo é de $saldoConta")
+                    }
+                }
+            0 -> { println("Saindo do sistema...")
+                    continue
+                }
+            else -> { println("Opção invalida")
+                }
+        }        
+    } while (opcao != 0)
+    println("programa encerrado")       
 }
