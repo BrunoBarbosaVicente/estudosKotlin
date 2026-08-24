be5321973069155ad3f368d28c71de8c3130e8cc
Mon4rka <brunobarbosavicente@hotmail.com>
2026-08-24 11:19:37 -0300

    feat: adiciona inicio do projeto final semana 4

--- projetos/semana4/bibliotecaDeFunçõesMatematicas.kt
+++ projetos/semana4/bibliotecaDeFunçõesMatematicas.kt
@@ -1,1 +1,51 @@
-fun 
+fun somar(vararg numeros: Int): Int {
+    var resultado: Int
+    for (numero in numeros){
+        resultado += numero
+    }
+    return resultado
+}
+//fun subtrair() {
+
+//}
+//fun multiplicar() {
+
+//}
+//fun dividir() {
+
+//}
+//fun media(vararg numeros: Double): Double {
+
+//}
+//fun maiorNumero(){
+
+//}
+//fun menorNumero() {
+
+//}
+//fun calcularArea() {
+
+//}
+//fun calcularArea() {
+
+//}
+fun imprimirMenu(){
+    println("===========================")
+    println("   Biblioteca Matemática   ")
+    println("===========================")
+    println("\n1 - Somar")
+    println("2 - Subtrair")
+    println("3 - Multiplicar")
+    println("4 - Dividir")
+    println("5 - Média")
+    println("6 - Maior número")
+    println("7 - Menor número")
+    println("8 - Calcular área")
+    println("0 - Sair")
+}
+fun main () {
+    imprimirMenu()
+    val entradaMenu = readln()
+    contador
+
+}
