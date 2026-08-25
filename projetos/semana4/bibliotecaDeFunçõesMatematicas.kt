aabf81a7483dac0a5ee41eb74e22f4b9c94b1a04
BrunoBarbosaVicente <brunobarbosavicente@hotmail.com>
2026-08-25 00:35:55 -0300

    feat: adiciona projeto final bibioteca de funções matematicas semana 4 concluida

--- projetos/semana4/bibliotecaDeFunçõesMatematicas.kt
+++ projetos/semana4/bibliotecaDeFunçõesMatematicas.kt
@@ -1,34 +1,46 @@
 fun somar(vararg numeros: Int): Int {
-    var resultado: Int
-    for (numero in numeros){
-        resultado += numero
+    var resultado = 0
+   for (numero in numeros){
+       resultado += numero
     }
     return resultado
 }
-//fun subtrair() {
-
-//}
-//fun multiplicar() {
-
-//}
-//fun dividir() {
-
-//}
-//fun media(vararg numeros: Double): Double {
-
-//}
-//fun maiorNumero(){
-
-//}
-//fun menorNumero() {
-
-//}
-//fun calcularArea() {
-
-//}
-//fun calcularArea() {
-
-//}
+fun subtrair(n1: Int, n2: Int): Int {
+    return n1 - n2
+}
+fun multiplicar(n1: Int, n2: Int): Int {
+    return n1 * n2
+}
+fun dividir(n1: Int, n2: Int): Int {
+    return n1 / n2
+}
+fun media(vararg numeros: Double): Double {
+    var resultado = 0.0
+    var contador = 0
+    for (numero in numeros){
+        resultado += numero
+        contador++
+    }
+    return resultado / contador
+}
+fun maiorNumero(n1: Int, n2: Int): Int {
+    if (n1 > n2){
+        return n1
+    }
+    return n2
+}
+fun menorNumero(n1: Int, n2: Int): Int {
+    if (n1 < n2){
+        return n1
+    }
+    return n2
+}
+fun calcularArea(n1: Double): Double {
+    return n1 * n1
+}
+fun calcularArea(n1: Double, n2: Double): Double {
+    return n1 * n2
+}
 fun imprimirMenu(){
     println("===========================")
     println("   Biblioteca Matemática   ")
@@ -44,8 +56,86 @@ fun imprimirMenu(){
     println("0 - Sair")
 }
 fun main () {
-    imprimirMenu()
-    val entradaMenu = readln()
-    contador
-
+    
+    do {
+        imprimirMenu()
+        println("Escolha uma opção: ")
+        val entradaMenu = readln().toInt()
+        when (entradaMenu) {
+            1 -> {
+                println("Digite o primeiro numero:")
+                val n1 = readln().toInt()
+                println("Digite o segundo numero:")
+                val n2 = readln().toInt()
+                val resultado = somar(n1, n2)
+                println("Resultado: $resultado")
+            }
+            2 -> {
+                println("Digite o primeiro numero:")
+                val n1 = readln().toInt()
+                println("Digite o segundo numero:")
+                val n2 = readln().toInt()
+                val resultado = subtrair(n1, n2)
+                println("Resultado: $resultado")
+            }
+            3 -> {
+                println("Digite o primeiro numero:")
+                val n1 = readln().toInt()
+                println("Digite o segundo numero:")
+                val n2 = readln().toInt()
+                val resultado = multiplicar(n1, n2)
+                println("Resultado: $resultado")
+            }
+            4 -> {
+                println("Digite o primeiro numero:")
+                val n1 = readln().toInt()
+                println("Digite o segundo numero:")
+                val n2 = readln().toInt()
+                val resultado = dividir(n1, n2)
+                println("Resultado: $resultado")
+            }
+            5 -> {
+                println("Digite o primeiro numero:")
+                val n1 = readln().toDouble()
+                println("Digite o segundo numero:")
+                val n2 = readln().toDouble()
+                val resultado = media(n1, n2)
+                println("Resultado: $resultado")
+            }
+            6 -> {
+                println("Digite o primeiro numero:")
+                val n1 = readln().toInt()
+                println("Digite o segundo numero:")
+                val n2 = readln().toInt()
+                val resultado = maiorNumero(n1, n2)
+                println("O numero maior é: $resultado")
+            }
+            7 -> {
+                println("Digite o primeiro numero:")
+                val n1 = readln().toInt()
+                println("Digite o segundo numero:")
+                val n2 = readln().toInt()
+                val resultado = menorNumero(n1, n2)
+                println("O numero menor é: $resultado")
+            }
+            8 -> {
+                println("Digite o primeiro numero:")
+                val n1 = readln().toDouble()
+                println("Digite o segundo numero:")
+                val n2 = readln().toDouble()
+                if (n2 == 0.0){
+                    val resultado = calcularArea(n1)
+                    println("A area do quadrado é: $resultado")
+                } else {
+                    val resultado = calcularArea(n1, n2)
+                    println("A area do quadrado é: $resultado")
+                }
+            }
+            0 -> {
+                println("Calculadora finalizada...")
+                break
+            }
+            else -> {println("Numero invalido...")}
+        }
+    } while (entradaMenu != 0) 
 }
