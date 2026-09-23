773fd528bc83a9ec97c6780cef905f2eb85d9c39
BrunoBarbosaVicente <brunobarbosavicente@hotmail.com>
2026-09-23 00:12:23 -0300

    feat: melhoria no projeto sistemaBancario senana 7

--- projetos/semana7/sistemaBancario.kt
+++ projetos/semana7/sistemaBancario.kt
@@ -1,22 +1,34 @@
 class ContaBancaria(titular: String){
     var saldo = 0.0
         private set
-        fun depositar (valor: Double){
+        fun depositar (valor: Double): Boolean{
+            var saida = false
             if (valor > 0){
                 saldo += valor
+                saida = true
             }
+            return saida
         }
-        fun sacar (valor: Double){
+        fun sacar (valor: Double): Boolean{
+            var saida = false
             if (valor > 0 && valor <= saldo){
                 saldo -= valor
+                saida = true
             }
+            return saida
         }
-        fun transferir (destino: ContaBancaria, valor: Double){
+        fun transferir (destino: ContaBancaria, valor: Double): Boolean{
+            var saida = false
             if (valor > 0 && valor <= saldo){
                 saldo -= valor
                 destino.depositar(valor)
+                saida = true
             }
+            return saida
         }
+}
+class Pessoa(var nome: String){
+    
 }
 fun menu(){
     println("====== B-Bank =====")
@@ -31,8 +43,16 @@ fun menu(){
 
 
 fun main () {
-    val conta1 = ContaBancaria("Bruno")
-    val conta2 = ContaBancaria("Carolina")
+    print("Digito o nome da sua conta: ")
+    val entradaNomeConta = readln()
+    val pessoa1 = Pessoa(entradaNomeConta)
+    print("Digite o nome da conta que deseja realizar transação: ")
+    val entradaNomeConta1 = readln()
+    val pessoa2 = Pessoa(entradaNomeConta1)
+    val conta1 = ContaBancaria(pessoa1.nome)
+    val conta2 = ContaBancaria(pessoa2.nome)
+    
+    
     do{
         menu()
         print("Digite a operação desejada: ")
@@ -43,13 +63,19 @@ fun main () {
                 val entradaConta = readln()
                 print("Valor o valor que deseja sacar: ")
                 val entradaSaque = readln().toDouble()
-                if (entradaConta == "Bruno"){
-                    conta1.sacar(entradaSaque)
-                    println("Saque realizado com sucesso.")
+                if (entradaConta == pessoa1.nome){
+                    if(conta1.sacar(entradaSaque)){
+                        println("Saque realizado.")
+                    } else {
+                        println("Saldo indisponivel.")
+                    }
                 }
-                if (entradaConta == "Carolina"){
-                    conta2.sacar(entradaSaque)
-                    println("Saque realizado com sucesso.")
+                if (entradaConta == pessoa2.nome){
+                    if (conta2.sacar(entradaSaque)){
+                        println("Saque realizado.")
+                    } else {
+                        println("Saldo indisponivel")
+                    }
                 }
             }
             2 -> {
@@ -57,13 +83,19 @@ fun main () {
                 val entradaConta = readln()
                 print("Qual o valor que deseja depositar: ")
                 val entradaValor = readln().toDouble()
-                if (entradaConta == "Bruno"){
-                    conta1.depositar(entradaValor)
-                    println("Deposito realizado com sucesso.")
+                if (entradaConta == pessoa1.nome){
+                    if (conta1.depositar(entradaValor)){
+                        println("Deposito realizado")
+                    } else {
+                        println("Deposito invalido, valor precisa ser maior que 0")
+                    }
                 }
-                if (entradaConta == "Carolina"){
-                    conta2.depositar(entradaValor)
-                    println("Deposito realizado com sucesso.")
+                if (entradaConta == pessoa2.nome){
+                    if (conta2.depositar(entradaValor)){
+                        println("Deposito realizado")
+                    } else {
+                        println("Deposito invalido, valor precisa ser maior que 0")
+                    }
                 }
             }
             3 -> {
@@ -71,21 +103,30 @@ fun main () {
                 val entradaValor = readln().toDouble()
                 print("Para quem vai transferir esse valor: ")
                 val entradaDestino = readln()
-                if (entradaDestino == "Bruno"){
-                    conta2.transferir(conta1, entradaValor)
+                if (entradaDestino == pessoa2.nome){
+                    if (conta2.transferir(conta1, entradaValor)){
+                        println("Transferencia realizada")
+                    } else {
+                        println("Saldo insuficiente")
+                    }
                 }
-                if (entradaDestino == "Carolina"){
-                    conta1.transferir(conta2, entradaValor)
+                if (entradaDestino == pessoa1.nome){
+                    if (conta1.transferir(conta2, entradaValor)){
+                        println("Transferencia realizada")
+                    } else {
+                        println("Saldo insuficiente")
+                    }
                 }
             }
             4 -> {
                 print("Qual conta deseja verificar o saldo: ")
                 val entradaConta = readln()
-                if (entradaConta == "Bruno"){
-                    println("Seu saldo é ${conta1.saldo}")
-                }
-                if (entradaConta == "Carolina"){
-                    println("Seu saldo é ${conta2.saldo}")
+                if (entradaConta == pessoa1.nome){
+                    println("Seu saldo de ${pessoa1.nome} é ${conta1.saldo}")
+                } else if (entradaConta == pessoa2.nome){
+                    println("Seu saldo de ${pessoa2.nome} é ${conta2.saldo}")
+                } else {
+                    println("Conta invalida.")
                 }
             }
             0 -> {
